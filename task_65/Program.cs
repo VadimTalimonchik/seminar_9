758a2b7dfe2ff3948aae7bd9c6f2e8d136e59711
VadimTalimonchik <tvadmins@gmail.com>
2022-12-25 10:57:03 +0300

    Задача 65

--- task_65/Program.cs
+++ task_65/Program.cs
@@ -4,4 +4,11 @@
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
 
-Console.WriteLine("Hello, World!");
+void NumbersFromMtoN(int m, int n)
+{
+    if (n > m) NumbersFromMtoN(m, n - 1);
+    Console.Write($"{n} ");
+    if (m > n) NumbersFromMtoN(m, n + 1);
+}
+NumbersFromMtoN(10, 1);
+
