c0093ad55ebb6d74b10eef74c7e3322c4b93867e
VadimTalimonchik <tvadmins@gmail.com>
2022-12-25 10:27:26 +0300

    Задача 63

--- task_63/Program.cs
+++ task_63/Program.cs
@@ -4,4 +4,35 @@
 // N = 5 -> "1, 2, 3, 4, 5"
 // N = 6 -> "1, 2, 3, 4, 5, 6"
 
-Console.WriteLine("Hello, World!");
+//Задача 63: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные
+// числа в промежутке от 1 до N.
+
+//ВАРИАНТ ВЫВОДА ЧЕРЕЗ СТРОЧНЫЙ МАССИВ
+string LineNumbers(int n)
+{
+return (n>=2) ? LineNumbers(n-1) + $" {n}" : "1";
+
+// if(n>=2) 
+// {
+//    return LineNumbers(n-1) + $" {n}";
+// }
+// else
+// {
+// return "1";
+// }
+
+}
+Console.WriteLine(LineNumbers(10));
+
+
+
+
+//ВАРИАНТ ВЫВОДА VOID
+void LineNumbers2(int n)
+{
+    if(n != 1) LineNumbers2(n - 1);
+    Console.Write($"{n} ");
+}
+LineNumbers2(5);
+
